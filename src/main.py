d0d893db34486dab3a2a6b32aa33775fee3308b1
Ben Jones 2 <ben.jones2@trustpayments.com>
2021-11-02 14:50:31 +0000

    before adding more realistic tests and records

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 """Runs either the cli or gui version of the program depending on AEVI_GUI environment variable."""
 from dotenv import load_dotenv
-import gui, cli
 from os import environ
 
 load_dotenv()
@@ -8,6 +7,8 @@ load_dotenv()
 GUI = environ.get("AEVI_GUI", "True").lower() in ("true", '1', 't')
 LOCAL = environ.get("AEVI_LOCAL", "True").lower() in ("true", '1', 't')
 if GUI:
+    import gui
     gui.run(LOCAL)
 else:
+    import cli
     cli.run(LOCAL)
